1174f5d4dd4abf43ec135be08c86fed58d5527d7
Kenichi Yokoshima <yokoshima@Kenichi-no-MacBook-Pro.local>
2019-08-23 17:34:54 +0700

    WIP

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -2,12 +2,13 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:noff/views/item.dart';
+import 'package:uuid/uuid.dart';
 import '../compoinents/target_list_storage.dart';
 
 class Home extends StatefulWidget {
+  const Home({Key key, @required this.storage}) : super(key: key);
   final TargetListStorage storage;
- 
-  Home({Key key, @required this.storage}) : super(key: key);
+
   @override
   State<StatefulWidget> createState() {
     return HomeState();
@@ -15,14 +16,12 @@ class Home extends StatefulWidget {
 }
 
 class HomeState extends State<Home> {
-  List<Map> _list = [];
+  final List<Map<String, dynamic>> _list = [];
   @override
   void initState() {
     super.initState();
-    SystemChrome.setPreferredOrientations([
-      DeviceOrientation.portraitUp,
-      DeviceOrientation.portraitDown
-    ]);
+    SystemChrome.setPreferredOrientations(
+        [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
     // widget.storage.readList().then((List<Map> list)
     //   {
     //     setState(() {
@@ -30,8 +29,9 @@ class HomeState extends State<Home> {
     //     });
     //   }
     // );
-    _list.add({ "name" : "aaaa"});
+    _list.add({'key': Uuid().v1(), 'name': 'aaaa', 'color': Colors.red});
   }
+
   @override
   void dispose() {
     SystemChrome.setPreferredOrientations([
@@ -42,33 +42,59 @@ class HomeState extends State<Home> {
     ]);
     super.dispose();
   }
-  Map<String, String> addList(Map<String, String> item) {
-    setState(() {_list.add(item); });
+
+  Map<String, dynamic> addList(Map<String, dynamic> item) {
+    setState(() {
+      _list.add(item);
+    });
   }
-  void removeItem(Map<String, String> item) {
-    setState(() { _list.remove(item); });
+
+  void removeItem(Map<String, dynamic> item) {
+    setState(() {
+      _list.remove(item);
+    });
   }
+
   @override
   Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
-    var padding = MediaQuery.of(context).padding;
-  
-    AppBar appBar = AppBar(title: Text('Text'),);
-    double height = size.height - padding.top - padding.bottom - appBar.preferredSize.height;
-    
+    final Size size = MediaQuery.of(context).size;
+    final EdgeInsets padding = MediaQuery.of(context).padding;
+
+    final AppBar appBar = AppBar(
+      title: const Text('Text'),
+    );
+    final double height = size.height -
+        padding.top -
+        padding.bottom -
+        appBar.preferredSize.height;
+
     return Scaffold(
       appBar: appBar,
       body: ListView.builder(
         itemBuilder: (BuildContext context, int index) {
-          var name = _list[index]['name'];
+          final Map<String, dynamic> item = _list[index];
+          final String name = item['name'];
+          final Color color = item['color'];
           return Card(
-            child: InkWell(
-              onTap: () {},
-              child: Padding(
-                padding: EdgeInsets.all(1.0),
-                child: Container(
-                  height: height / 5,
-                  child: Text('$name', style: TextStyle(fontSize: 42.0),),
+            color: color,
+            child: Dismissible(
+              key: Key(index.toString()),
+              onDismissed: (direction) {
+                setState(() {
+                  removeItem(item);
+                });
+              },
+              child: InkWell(
+                onTap: () {},
+                child: Padding(
+                  padding: const EdgeInsets.all(1.0),
+                  child: Container(
+                    height: height / 5,
+                    child: Text(
+                      '$name',
+                      style: TextStyle(fontSize: 42.0),
+                    ),
+                  ),
                 ),
               ),
             ),
@@ -78,14 +104,18 @@ class HomeState extends State<Home> {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          print("FAB press");
-          Navigator.push(context, MaterialPageRoute(builder: (context) => Item(parent: this)));
-          // Navigator.push(context, 
+          print('FAB press');
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (BuildContext context) => Item(parent: this),
+                maintainState: false,
+                fullscreenDialog: true),
+          );
+          // Navigator.push(context,
           // CupertinoPageRoute(fullscreenDialog: true, builder: (context) => Item(parent: this)));
-          
         },
       ),
     );
   }
 }
-
--- lib/views/item.dart
+++ lib/views/item.dart
@@ -1,30 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_colorpicker/block_picker.dart';
+import 'package:flutter_colorpicker/flutter_colorpicker.dart';
 
 import 'home.dart';
 
+@immutable
 class Item extends StatelessWidget {
-
+  const Item({Key key, @required this.parent}) : super(key: key);
   final HomeState parent;
-  Item({Key key, @required this.parent}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Second route"),),
-      body: RaisedButton(
-        onPressed:() {
-           Navigator.pop(context);
-        },
-        child: ItemForm(this.parent),
+      appBar: AppBar(
+        title: const Text('Second route'),
       ),
+      body: ItemForm(parent),
     );
   }
-  
 }
 
+@immutable
 class ItemForm extends StatefulWidget {
+  const ItemForm(this.parent);
   final HomeState parent;
-  ItemForm(this.parent);
   @override
   State<StatefulWidget> createState() {
     return ItemFormState();
@@ -32,45 +31,59 @@ class ItemForm extends StatefulWidget {
 }
 
 class ItemFormState extends State<ItemForm> {
-  String _name = "";
-  final _nameController = TextEditingController();
+  final TextEditingController _nameController = TextEditingController();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  Color itemColor = Colors.blue;
   @override
   Widget build(BuildContext context) {
-    final _formKey = GlobalKey<FormState>();
-    return Form(
-        key: _formKey,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            TextFormField(
-              validator: (value) {
-                if (value.isEmpty) {
-                  return "Please enter some text";
-                }
-                return null;
-              },
-              controller: _nameController,
-            ),
-            Padding(
-              padding: EdgeInsets.only(top: 25.0),
-              child: RaisedButton(
-                onPressed: () {
-                  if (_formKey.currentState.validate()) {    
-                    this.widget.parent.addList({"name": "bbbbb"});
-                    Navigator.pop(context);
-                  } 
-                },
-                child: Text('Submit'),
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.only(top: 100.0)
-            ),
-          ],
-          )
-
-    );
+    final double width = MediaQuery.of(context).size.width;
+    final double height = MediaQuery.of(context).size.height;
+    return Center(
+        child: Container(
+            width: width / 3 * 2,
+            child: Form(
+                key: _formKey,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 20.0),
+                      child: TextFormField(
+                        validator: (String value) {
+                          if (value.isEmpty) {
+                            return 'Please enter some text';
+                          }
+                          return null;
+                        },
+                        controller: _nameController,
+                      ),
+                    ),
+                    Container(
+                      // color: Colors.cyan,
+                      child: BlockPicker(
+                        onColorChanged: (Color value) {
+                          setState(() {
+                            itemColor = value;
+                          });
+                        },
+                        pickerColor: itemColor,
+                      ),
+                      height: height / 2.0,
+                    ),
+                    RaisedButton(
+                      onPressed: () {
+                        if (_formKey.currentState.validate()) {
+                          widget.parent.addList({
+                            'name': _nameController.text,
+                            'color': itemColor
+                          });
+                          Navigator.pop(context);
+                        }
+                      },
+                      child: const Text('Submit'),
+                    ),
+                  ],
+                ))));
   }
-  
 }
